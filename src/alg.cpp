82fd19b10d4523e2d37e2ccbcd93871546682657
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-05-28 18:52:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,12 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+  std::string s = tree[n - 1];
+  std::vector<char> perm;
+  int x = 0;
+  while (x < s.length()) {
+    perm.push_back(s[x]);
+    ++x;
+  }
+  return perm;
 }
